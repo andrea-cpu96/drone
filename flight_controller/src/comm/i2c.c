b46c9be4c89d2822c9611891d40c67791386fab7
Andrea <andrea.1.fato@electrolux.com>
2026-07-13 15:18:59 +0200

    Add i2c driver APIs implementation

--- flight_controller/src/comm/i2c.c
+++ flight_controller/src/comm/i2c.c
@@ -1,7 +1,67 @@
 #include "i2c.h"
 
+#include <errno.h>
+#include <stdio.h>
+
 #include <zephyr/drivers/i2c.h>
 
 const struct device *const i2c_driver =
     DEVICE_DT_GET(DT_ALIAS(i2c_interface));
 
+void i2c_master_init(void)
+{
+    if (!device_is_ready(i2c_driver))
+    {
+        printf("I2C device not ready\n");
+    }
+}
+
+enum status_code i2c_master_write_packet_wait(struct i2c_master_packet *const packet)
+{
+    int ret = i2c_write(i2c_driver, packet->data, packet->data_length,
+                        packet->address);
+    if (ret == 0)
+    {
+        return STATUS_OK;
+    }
+    if (ret == -EIO)
+    {
+        return STATUS_ERR_OVERFLOW;
+    }
+    return STATUS_ERR_TIMEOUT;
+}
+
+enum status_code i2c_master_write_packet_wait_no_stop(struct i2c_master_packet *const packet)
+{
+    struct i2c_msg msg = {
+        .buf = packet->data,
+        .len = packet->data_length,
+        .flags = I2C_MSG_WRITE,
+    };
+
+    int ret = i2c_transfer(i2c_driver, &msg, 1, packet->address);
+    if (ret == 0)
+    {
+        return STATUS_OK;
+    }
+    if (ret == -EIO)
+    {
+        return STATUS_ERR_OVERFLOW;
+    }
+    return STATUS_ERR_TIMEOUT;
+}
+
+enum status_code i2c_master_read_packet_wait(struct i2c_master_packet *const packet)
+{
+    int ret = i2c_read(i2c_driver, packet->data, packet->data_length,
+                       packet->address);
+    if (ret == 0)
+    {
+        return STATUS_OK;
+    }
+    if (ret == -EIO)
+    {
+        return STATUS_ERR_OVERFLOW;
+    }
+    return STATUS_ERR_TIMEOUT;
+}
--- flight_controller/src/comm/i2c.h
+++ flight_controller/src/comm/i2c.h
@@ -1,8 +1,33 @@
 #ifndef I2C_H
 #define I2C_H
 
+#include <stdint.h>
+
 #include <zephyr/device.h>
 
-extern const struct device *const i2c_driver;
+enum i2c_transfer_direction {
+	I2C_TRANSFER_WRITE = 0,
+	I2C_TRANSFER_READ  = 1,
+};
+
+enum status_code {
+	STATUS_OK           = 0x00,
+	STATUS_ERR_OVERFLOW = 0x01,
+	STATUS_ERR_TIMEOUT  = 0x02,
+};
+
+struct i2c_master_packet {
+	/* Address to slave device */
+	uint16_t address;
+	/* Length of data array */
+	uint16_t data_length;
+	/* Data array containing all data to be transferred */
+	uint8_t *data;
+};
+
+void i2c_master_init(void);
+enum status_code i2c_master_write_packet_wait(struct i2c_master_packet *const packet);
+enum status_code i2c_master_write_packet_wait_no_stop(struct i2c_master_packet *const packet);
+enum status_code i2c_master_read_packet_wait(struct i2c_master_packet *const packet);
 
 #endif /* I2C_H */
